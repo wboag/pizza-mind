972d8023a269722dd2be464dc6407e940d11c182
William G. Boag <wboag@safar.csail.mit.edu>
2017-05-26 13:04:23 -0400

    point to stable wiki dump from March 20

--- data/build.sh
+++ data/build.sh
@@ -1,5 +1,5 @@
 
-time wget https://dumps.wikimedia.org/enwiki/20170101/enwiki-20170101-pages-articles-multistream.xml.bz2
+time wget http://text-machine.cs.uml.edu/willie/enwiki-20170320-pages-articles-multistream.xml.bz2
 
-time bzip2 -c -d enwiki-latest-pages-articles.xml.bz2 | awk '{print tolower($0);}' | perl wikifil.pl | bash normalize_text.sh | awk '{if (NF>1) print;}' > data.txt
+time bzip2 -c -d enwiki-20170320-pages-articles-multistream.xml.bz2 | awk '{print tolower($0);}' | perl wikifil.pl | bash normalize_text.sh | awk '{if (NF>1) print;}' > data.txt
 
